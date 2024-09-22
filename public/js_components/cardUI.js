6a92ea9028ecc7690187df5e03664118acb7e26d
Chris Angel <welovethefish0@gmail.com>
2024-09-22 16:36:15 +0200

    update 10

--- public/js_components/cardUI.js
+++ public/js_components/cardUI.js
@@ -10,8 +10,14 @@ const cardUI = function () {
     const markUp = `
                           
                           <div class="desert-card" id="${item.id}">
-                              <img src="${item.images.desktop}" class="desert-card_image ${item.quantity ? "border" : ""}" />
-                              <div class="desert-card_add-to-image ${item.quantity ? "hidden" : ""}">
+                              <img src="${
+                                item.images.desktop
+                              }" class="desert-card_image ${
+      item.quantity ? "border" : ""
+    }" />
+                              <div class="desert-card_add-to-image ${
+                                item.quantity ? "hidden" : ""
+                              }">
                                   <svg xmlns="http:www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20">
                                       <g fill="#C73B0F" clip-path="url(#a)">
                                           <path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/>
@@ -19,7 +25,9 @@ const cardUI = function () {
                                       </g>
                                   </svg><span>Add to Cart</span>
                               </div>
-                              <div class="desert-card_counter-container ${item.quantity ? "" : "hidden"}">
+                              <div class="desert-card_counter-container ${
+                                item.quantity ? "" : "hidden"
+                              }">
                               <svg class="decrease" xmlns="http://www.w3.org/2000/svg" viewBox="-7 -10 26 22">
                                   <circle cx="5" cy="1" r="8"></circle>
                                   <path d="M0 .375h10v1.25H0V.375Z"/></svg>
@@ -32,13 +40,20 @@ const cardUI = function () {
                               <div class="desert-card_description">
                                   <ul class="desert-card_description_list">
                                       <li class="desert-card_description_list_item">
-                                          <p class="item-title">${item.category}</p>
+                                          <p class="item-title">${
+                                            item.category
+                                          }</p>
                                       </li>
                                       <li class="desert-card_description_list_item">
-                                          <h4 class="description">${item.name}</h4>
+                                          <h4 class="description">${
+                                            item.name
+                                          }</h4>
                                       </li>
                                       <li class="desert-card_description_list_item">
-                                          <p class="price">${euro(item.price) + euro(item.price).s.symbol}</p>
+                                          <p class="price">${
+                                            euro(item.price) +
+                                            euro(item.price).s.symbol
+                                          }</p>
                                       </li>
                                   </ul>
                               </div>
